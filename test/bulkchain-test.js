35909339a404cb23d1b72d9569eb73e0bb61bf08
= <m0dul0@blockchain.army>
2015-07-29 07:53:49 -0700

    new mocha test

---
+++ test/bulkchain-test.js
@@ -0,0 +1,28 @@
+//'use strict';
+
+process.env.NODE_ENV = 'test';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+suite('bulkchain:', function(done) {
+    
+    function check( done, f ) {
+      try {
+        f()
+        done()
+      } catch( e ) {
+        done( e )
+      }
+    };
+    var config = require(__dirname + '/../config/options.js');
+    var assert = require("assert");
+    test('this always passes', function() {
+        assert.equal(1, 1);
+    });
+    test('Testing with callback (asynchronous)', function (done) {
+        setTimeout(completeWhenThisExecutes, 100)
+        function completeWhenThisExecutes() {
+            done();
+        }
+    });
+});
+
