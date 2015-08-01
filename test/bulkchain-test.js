66683207a1c92a8fd85adb249c0941d12a8fe47a
= <m0dul0@blockchain.army>
2015-07-31 23:24:22 -0700

    new tests, new jsdoc, made many changes related to use strict

--- test/bulkchain-test.js
+++ test/bulkchain-test.js
@@ -2,11 +2,11 @@
 
 process.env.NODE_ENV = 'test';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-var bulkchain = require(process.cwd() + '/lib/bulkchain.js')
-
-var assert = require("assert");
 
 suite('bulkchain:', function(done) {
+    
+    var bulkchain = require(process.cwd() + '/lib/bulkchain.js')
+    var assert = require("assert");
     
     function check( done, f ) {
       try {
@@ -16,13 +16,39 @@ suite('bulkchain:', function(done) {
         done( e )
       }
     }
-
-    test('date_to_blockcount with a known date', function (done) {
-        var targetdate = 1435734000
-        date_to_blockcount(targetdate, function(blockinfo) {
-            assert.equal(blockinfo.blockcount, 363312)
+    test('getBlockCount', function (done) {
+        bulkchain.getBlockCount(function (blockcount) {
+            assert(blockcount > 363312)
             done()
         })
     });
-});
+    test('dateToBlockCount (pre-satoshi targetdate)', function (done) {
+        bulkchain.dateToBlockCount(100, function (blockcount) {
+            assert(blockcount < 363312)
+            done()
+        })
+    });
+    test('dateToBlockCount (magic)', function (done) {
+        bulkchain.dateToBlockCount(1435734000, function (blockcount) {
+            assert(blockcount = 363313)
+            done()
+        })
+    });
+    
+    test('getBlock (magic)', function (done) {
+        var blockhash = '00000000000000000bb70c518539844d0b35b30c2c785413881c0de37eb00d38'
+        bulkchain.getBlock(blockhash, function (block) {
+            assert.equal(blockhash, block.hash)
+            done()
+        })
+    })
+    
+    test('getBlockTime (magic numbers)', function (done) {
+        var blockhash = '000000000000000004c7154fec6527603c642b3622803c7de06dd18ec56e4894'
+        bulkchain.getBlockTime(blockhash, function (blocktime) {
+            assert.equal(blocktime, 1435733095)
+            done()
+        })
+    })
+})
 
