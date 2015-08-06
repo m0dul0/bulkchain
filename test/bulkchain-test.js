030397be869bdf15e1b1c9af09c5a4fc3e301fc6
= <m0dul0@blockchain.army>
2015-08-05 21:33:57 -0700

    add dateRangeToBlockHash function

--- test/bulkchain-test.js
+++ test/bulkchain-test.js
@@ -4,10 +4,10 @@ process.env.NODE_ENV = 'test';
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 suite('bulkchain:', function(done) {
-    
+
     var bulkchain = require(process.cwd() + '/lib/bulkchain.js')
     var assert = require("assert");
-    
+
     function check( done, f ) {
       try {
         f()
@@ -16,14 +16,14 @@ suite('bulkchain:', function(done) {
         done( e )
       }
     }
-    
+
     test('getBlockCount', function(done) {
         bulkchain.getBlockCount( function client_getblockcount (err, blockcount) {
             assert(blockcount !== undefined)
             done()
         })
     })
-    
+
     test('blockCountToBlockHash', function (done) {
         bulkchain.blockCountToBlockHash(363312, cb_blockCountToBlockHash)
         function cb_blockCountToBlockHash(err, blockhash) {
@@ -31,19 +31,19 @@ suite('bulkchain:', function(done) {
             done()
         }
     })
-    
+
     test('blockHashToBlockHeader (magic)', function (done) {
         var blockhash = '000000000000000004c7154fec6527603c642b3622803c7de06dd18ec56e4894'
-        bulkchain.blockHashToBlockHeader(blockhash, cb_getBlock) 
+        bulkchain.blockHashToBlockHeader(blockhash, cb_getBlock)
         function cb_getBlock(err, blockheader) {
             assert.equal(blockheader.hash, '000000000000000004c7154fec6527603c642b3622803c7de06dd18ec56e4894')
             done()
         }
     })
-    
+
     test('blockCountToTime (magic)', function (done) {
         var blockcount = 367640
-        bulkchain.blockCountToTime(blockcount, cb_blockCountToTime) 
+        bulkchain.blockCountToTime(blockcount, cb_blockCountToTime)
         function cb_blockCountToTime(err, time) {
             assert.equal(time, 1438263884)
             done()
@@ -64,7 +64,7 @@ suite('bulkchain:', function(done) {
             done()
         }
      })
-    
+
     test('dateToBlockCount (post-apocalypse)', function (done) {
         var targettime = 9999999999
         bulkchain.dateToBlockCount(targettime, cb_dateToBlockcount)
@@ -73,7 +73,7 @@ suite('bulkchain:', function(done) {
             done()
         }
     })
-    
+
     test('dateToBlockCount (targettime == blocktime)', function (done) {
         var targettime = 1438656758
         bulkchain.dateToBlockCount(targettime, cb_dateToBlockcount)
@@ -82,7 +82,7 @@ suite('bulkchain:', function(done) {
             done()
         }
     })
-    
+
     test('dateToBlockCount (targettime != blocktime)', function (done) {
         var targettime = 1438656757
         bulkchain.dateToBlockCount(targettime, cb_dateToBlockcount)
@@ -91,5 +91,26 @@ suite('bulkchain:', function(done) {
             done()
         }
     })
+    
+    test('dateRangeToBlockHash (targettime == blocktime)', function (done) {
+        var starttime = 1438825753 // 368590
+        var endtime =   1438830991  //368596
+        bulkchain.dateRangeToBlockHash(starttime, endtime, cb_dateRangeToBlockHash)
+        function cb_dateRangeToBlockHash (blockhasharr) {
+            assert.equal(blockhasharr.length, 7)
+            done()
+        }
+    })
+    
+    test('dateRangeToBlockHash (targettime != blocktime)', function (done) {
+        var starttime = 1438825700 // 368590
+        var endtime =   1438830900  //368596
+        bulkchain.dateRangeToBlockHash(starttime, endtime, cb_dateRangeToBlockHash)
+        function cb_dateRangeToBlockHash (blockhasharr) {
+            assert.equal(blockhasharr.length, 7)
+            done()
+        }
+    })
+
 })
 
