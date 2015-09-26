a60ff3c8f752491408cabf7312645feabd4a60f2
Cole Albon <cole@blockchain.army>
2015-09-26 15:50:56 -0700

    add date range (not tested for dollars yet)

--- README.md
+++ README.md
@@ -17,6 +17,9 @@ node based utilities for systems integration
     var bulkchain = require(process.cwd() + '/lib/bulkchain.js')  
 (plenty of library examples in the test suite [bulkchain-test.js](https://github.com/m0dul0/bulkchain/blob/master/test/bulkchain-test.js))  
 ###bulkchain-cli
+####OSX
     ./bulkchain-cli.js datetoblockcount --targetdate `date -j -f %Y%m%d%H%M%S 20150701000000 +%s`  
     ./bulkchain-cli.js datetoblockhash --startdate `date -j -f %Y%m%d%H%M%S 20150701000000 +%s` --enddate `date -j -f %Y%m%d%H%M%S 20150702000000 +%s`
-
+####linux
+    ./bulkchain-cli.js datetoblockcount --targetdate `date --date "2015-07-02 00:00:00" +%s`
+    ./bulkchain-cli.js datetoblockhash --startdate `date --date "2015-07-01 00:00:00" +%s` --enddate `date --date "2015-07-02 00:00:00" +%s`
--- bulkchain-cli.js
+++ bulkchain-cli.js
@@ -35,6 +35,24 @@ var argv = yargs
           .help('help')
           .argv
         })
+      
+      .command('datetotransactionsignature', 'get transaction level signatures between startdate and enddate', function (yargs) {
+          argv = yargs.options({
+              startdate: {
+                  description: "date in unixtime (+%s)\nexamples:\n`date +%s`\n`date -j -f %Y%m%d%H%M%S 20150701000000 +%s`", 
+                               requiresArg: true,
+              }
+              ,enddate: {
+                  description: "date in unixtime (+%s)\nexamples:\n`date +%s`\n`date -j -f %Y%m%d%H%M%S 20150702000000 +%s`", 
+                               requiresArg: true,
+              }
+          })
+          .demand('startdate', 'enddate')
+          .example('$0 datetoblockhash --startdate `date -j -f %Y%m%d%H%M%S 20150701000000 +%s` --enddate `date -j -f %Y%m%d%H%M%S 20150702000000 +%s`', 'get all blockhashes on July 1 local timezone')
+          .help('help')
+          .argv
+      })
+      
     .demand(1)
     .help('help').alias('help', 'h')
     .version('1.0.0', 'version').alias('version', 'V')
@@ -55,5 +73,11 @@ if (argv._[0] === 'datetoblockcount') {
 if (argv._[0] === 'datetoblockhash') {
     bulkchain.dateRangeToBlockHash(argv.startdate, argv.enddate, function (blockhashlist) {
         console.log(JSON.stringify(blockhashlist))
+    })
+}
+
+if (argv._[0] === 'datetotransactionsignature') {
+    bulkchain.dateRangeToTransactionSignature(argv.startdate, argv.enddate, function (transactionSignatureArr) {
+        console.log(JSON.stringify(transactionSignatureArr))
     })
 }
